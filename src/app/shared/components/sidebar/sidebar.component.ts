62796abc1ebabc90bb28956433c6ebed49480ec4
Claudio-Olivera <claudioolivera753@gmail.com>
2023-07-07 17:19:06 -0300

    end section 10

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+import { Route, Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent {
+
+  /* routes: Route[];
+
+  constructor(private router: Router) {
+    this.routes = this.router.config.filter(route => route.path !== '**');
+  } */
+
+}
